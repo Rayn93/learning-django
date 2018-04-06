a3416250c9fc4ede85d8674ee88e3d84930ca0c6
Robert Saternus <robert.saternus@gmail.com>
2018-04-06 12:37:18 +0200

    Working with templates I

--- biblo/urls.py
+++ biblo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+# from .views import AuthorListView
+from shelf.views import AuthorListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^authors/$', AuthorListView.as_view())
 ]
--- shelf/views.py
+++ shelf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Autor
+
+
 
 # Create your views here.
+
+class AuthorListView(ListView):
+    model = Autor
+
