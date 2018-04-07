1778598fc38b1b8f8e919d49e52ca795d0cb71a2
Robert Saternus <robert.saternus@gmail.com>
2018-04-07 23:10:23 +0200

    Renal app start + model

--- shelf/models.py
+++ shelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
 
@@ -6,12 +8,17 @@ from django.db import models
 
 class Autor(models.Model):
 
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(verbose_name=_("first name"), max_length=20)
+    last_name = models.CharField(verbose_name=_("last name"),max_length=50)
 
     def __str__(self):
         return "{first_name} {last_name}".format(first_name=self.first_name, last_name=self.last_name)
 
+
+    class Meta:
+        ordering = ('last_name', 'first_name')
+        verbose_name = _('author')
+        verbose_name_plural = _('authors')
 
 
 
